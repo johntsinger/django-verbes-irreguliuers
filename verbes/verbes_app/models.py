32cbb834ee4660826a449b8a52be94c1d20da8a3
John <john.t.singer@gmail.com>
2022-04-27 12:18:09 +0200

    Creation du model verbes

--- verbes/verbes_app/models.py
+++ verbes/verbes_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Verbes(models.Model):
+    present = models.fields.CharField(max_length=50, blank=True)
+    preterit = models.fields.CharField(max_length=50, blank=True)
+    participe_passe = models.fields.CharField(max_length=50, blank=True)
+    francais = models.fields.CharField(max_length=50)
