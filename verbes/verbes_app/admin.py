c01b1fa92b317b18c7408e023d9a91c8b1c09b02
John <john.t.singer@gmail.com>
2022-04-27 13:04:03 +0200

    Creation du model Verbe, de ses migrations, ajout des élément a la base de données et creation du super user modification de l'affichage dans la page d'admin

--- verbes/verbes_app/admin.py
+++ verbes/verbes_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from verbes_app.models import Verbe
 
-# Register your models here.
+
+class VerbeAdmin(admin.ModelAdmin):
+    list_display = ('francais', 'present', 'preterit', 'participe_passe')
+
+admin.site.register(Verbe, VerbeAdmin)
--- verbes/verbes_app/models.py
+++ verbes/verbes_app/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
-class Verbes(models.Model):
+class Verbe(models.Model):
     present = models.fields.CharField(max_length=50, blank=True)
     preterit = models.fields.CharField(max_length=50, blank=True)
     participe_passe = models.fields.CharField(max_length=50, blank=True)
     francais = models.fields.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.francais}'
