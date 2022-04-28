8166dc2aad396f39e8cdff010cc5c8f9ac82090c
John <john.t.singer@gmail.com>
2022-04-28 18:00:21 +0200

    pas compris comment faire du coup commit ici

--- verbes/verbes_app/views.py
+++ verbes/verbes_app/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render
-<<<<<<< Updated upstream
-from verbes_app.models import Verbe
-
-=======
-<<<<<<< Updated upstream
-
-# Create your views here.
-=======
 from verbes_app.models import Verbe, Table
 
->>>>>>> Stashed changes
 
 def verbe_list(request):
     verbes = Verbe.objects.all()
     return render(request,
         "verbes_app/verbe_list.html",
         {"verbes": verbes})
-<<<<<<< Updated upstream
-=======
 
 def table_detail(request, table_id):
     table = Table.objects.get(id=table_id)
@@ -30,5 +19,3 @@ def table_list(request):
     return render(request,
         "verbes_app/table_list.html",
         {"tables": tables})
->>>>>>> Stashed changes
->>>>>>> Stashed changes
