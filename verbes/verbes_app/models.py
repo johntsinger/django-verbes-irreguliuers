727a4346a616afaa13c8f9943745a260c636d974
John <john.t.singer@gmail.com>
2022-05-04 17:46:59 +0200

    Mise a jour du model Verbe pour ajouter deux boolean fields done et success et creation des migrations

--- verbes/verbes_app/models.py
+++ verbes/verbes_app/models.py
@@ -6,10 +6,12 @@ class Verbe(models.Model):
     preterit = models.fields.CharField(max_length=50, blank=True)
     participe_passe = models.fields.CharField(max_length=50, blank=True)
     francais = models.fields.CharField(max_length=50)
+    done = models.fields.BooleanField(default=False)
+    success = models.fields.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.present} {self.preterit} {self.participe_passe} \
-        {self.francais}'
+{self.francais}'
 
 
 class Table(models.Model):
