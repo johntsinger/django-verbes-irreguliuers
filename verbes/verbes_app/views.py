d1e98fc5807656dd81629553ab4a95f8cb21e394
John <john.t.singer@gmail.com>
2022-05-04 17:54:56 +0200

    Creation des urls, des vues et des pages html pour exercise et exercise_result

--- verbes/verbes_app/views.py
+++ verbes/verbes_app/views.py
@@ -1,6 +1,8 @@
+from random import sample
 from django.shortcuts import render
+from verbes_app.functions import get_results, verify_answer
 from verbes_app.models import Verbe, Table
-from verbes_app.forms import TableForm
+from verbes_app.forms import TableForm, VerbeForm
 from django.shortcuts import redirect
 
 
@@ -24,11 +26,13 @@ def table_list(request):
 
 def table_create(request):
     if request.method == "POST":
+        # Button envoyer
         if 'envoyer' in request.POST:
             form = TableForm(request.POST)
             if form.is_valid():
                 table = form.save()
                 return redirect('table-detail', table.id)
+        # Button annuler
         else:
             return redirect('table-list')
     else:
@@ -37,3 +41,35 @@ def table_create(request):
     return render(request,
         'verbes_app/table_create.html',
         {'form': form})
+
+def exercise(request, table_id):
+    table = Table.objects.get(id=table_id)
+    # gets 10 random items of table.verbes
+    verbes_list = list(table.verbes.all())
+    verbes = sample(verbes_list, 10)
+    # creates list of verbes id, need to pass it to context to get it in request.POST
+    verbes_id = [verbe.id for verbe in verbes]
+    form = VerbeForm()
+                 
+    return render(request,
+        "verbes_app/exercise.html",
+        {"table": table, "verbes": verbes, "form": form, 
+        'verbes_id': verbes_id})
+
+def exercise_result(request, table_id): 
+    results = get_results(request)
+    correction = verify_answer(results)
+    n = len(results['verbes'])
+
+    for i, verbe in enumerate(results['verbes']):
+        verbe.done = True
+        verbe.success = correction[i][3]
+        verbe.save()
+
+    table = Table.objects.get(id=3)
+    for verbe in table.verbes.all():
+        print(verbe.done)
+
+    return render(request,
+        "verbes_app/exercise_result.html",
+        {'results': results, 'n': range(n), 'correction': correction})
