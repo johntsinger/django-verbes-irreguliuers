9dc616b2cae45eaf560da691f7a8d2e4417acbab
John <john.t.singer@gmail.com>
2022-05-09 19:39:21 +0200

    Ajout d'un serializer json pour le querrydict Verbes.object.all()

--- verbes/verbes_app/views.py
+++ verbes/verbes_app/views.py
@@ -1,4 +1,5 @@
 from random import sample
+from django.core import serializers
 from django.shortcuts import render
 from django.shortcuts import redirect
 from verbes_app.functions import get_results, verify_answer
@@ -8,15 +9,9 @@ from verbes_app.forms import TableForm, VerbeForm
 
 def verbe_list(request):
     verbes = Verbe.objects.all()
-    verbes_tested = Verbe.objects.filter(done=True).count()
-    verbes_success = Verbe.objects.filter(success=True).count()
-    verbes_not_tested = verbes.count() - verbes_tested
-
     return render(request,
         "verbes_app/verbe_list.html",
-        {"verbes": verbes, 'verbes_tested': verbes_tested,
-         'verbes_success': verbes_success,
-         'verbes_not_tested': verbes_not_tested})
+        {"verbes": verbes})
 
 def table_detail(request, table_id):
     table = Table.objects.get(id=table_id)
@@ -31,6 +26,9 @@ def table_list(request):
         {"tables": tables})
 
 def table_create(request):
+    # creates a json dict of object Verbe with selected fields
+    verbes = serializers.serialize('json', Verbe.objects.all(),
+        fields=('done', 'success'))
     if request.method == "POST":
         # Button envoyer
         if 'envoyer' in request.POST:
@@ -46,14 +44,15 @@ def table_create(request):
 
     return render(request,
         'verbes_app/table_create.html',
-        {'form': form})
+        {'form': form, 'verbes': verbes})
 
 def exercise(request, table_id):
     table = Table.objects.get(id=table_id)
     # gets 10 random items of table.verbes
     verbes_list = list(table.verbes.all())
     verbes = sample(verbes_list, 10)
-    # creates list of verbes id, need to pass it to context to get it in request.POST
+    # creates list of verbes id
+    # used in the hidden input to get it in request.POST
     verbes_id = [verbe.id for verbe in verbes]
     form = VerbeForm()
                  
@@ -62,11 +61,17 @@ def exercise(request, table_id):
         {"table": table, "verbes": verbes, "form": form, 
         'verbes_id': verbes_id})
 
-def exercise_result(request, table_id): 
+def exercise_result(request, table_id):
+    # creates the dictionary of results
     results = get_results(request)
+    # gets the correction (dict of booleans)
     correction = verify_answer(results)
+    # set n to pass it to the context to creates a range object
+    # needed to creates 10 rows in the template
     n = len(results['verbes'])
 
+    # modifies done and success attributes of verb object
+    # saves the modification in the db
     for i, verbe in enumerate(results['verbes']):
         verbe.done = True
         verbe.success = correction[i][3]
@@ -74,4 +79,5 @@ def exercise_result(request, table_id):
 
     return render(request,
         "verbes_app/exercise_result.html",
-        {'results': results, 'n': range(n), 'correction': correction})
+        {'results': results, 'n': range(n), 'correction': correction,
+         'table_id': table_id})
