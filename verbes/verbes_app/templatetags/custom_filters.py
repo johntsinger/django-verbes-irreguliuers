87a969c3e80c6e7805fa0999b086667bfce16d2c
John <john.t.singer@gmail.com>
2022-05-12 20:10:13 +0200

    Ajout de la documentation dans custom_filters.py

--- verbes/verbes_app/templatetags/custom_filters.py
+++ verbes/verbes_app/templatetags/custom_filters.py
@@ -12,18 +12,22 @@ def index(list_item, i):
 
 @register.filter
 def replace(item, characters):
+    """Add a space of each side of a given character if it is in the item"""
     if characters in item:
         return item.replace(characters, " " + characters + " ")
     return item
 
 @register.filter
 def count_done(item):
+    """Count the number of item that have the done=True attribute"""
     return item.filter(done=True).count()
 
 @register.filter
 def count_success(item):
+    """Count the number of item that have the success=True attribute"""
     return item.filter(success=True).count()
 
 @register.filter
 def count_remainders(item):
+    """Count the number of item that are not done"""
     return item.all().count() - count_done(item)
