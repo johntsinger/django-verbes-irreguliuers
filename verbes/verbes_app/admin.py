d8295db3ac44e447aaaabae9b82d9f4f53a16cb0
John <john.t.singer@gmail.com>
2022-05-20 11:31:38 +0200

    Creation de TableAdminForm qui gere le formulaire dans TableAdmin pour ajouter la relation m2m via la table de jonction et garder la forme de filter_horizontal (FilteredSelectMultiple) sinon affiche une liste des verbe et pas un tableau de selection, ajout du formulaire dans TableAdmin

--- verbes/verbes_app/admin.py
+++ verbes/verbes_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from verbes_app.models import Verbe, Table
+from django import forms
+from verbes_app.models import Verbe, Table, VerbeList
+from django.contrib.admin.widgets import FilteredSelectMultiple
 
 
 class VerbeAdmin(admin.ModelAdmin):
@@ -7,9 +9,43 @@ class VerbeAdmin(admin.ModelAdmin):
                     'done', 'success')
 
 
+class VerbeListInline(admin.TabularInline):
+    model = VerbeList
+
+
+class TableAdminForm(forms.ModelForm):
+    class Meta:
+        model = Table
+        fields = ['name', 'verbes', 'default']
+
+    verbes = forms.ModelMultipleChoiceField(
+        queryset=Verbe.objects.all(),
+        required=False,
+        widget=FilteredSelectMultiple(
+            verbose_name='Verbes',
+            is_stacked=False
+        )
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(TableAdminForm, self).__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields['verbes'].initial = self.instance.table_verbes.all()
+
+    def save(self, commit=True):
+        verbe_list = super(TableAdminForm, self).save(commit=False)
+        if commit:
+            verbe_list.save()
+
+        if verbe_list.pk:
+            verbe_list.verbe_set = self.cleaned_data['verbes']
+            self.save_m2m()
+
+        return verbe_list
+
+
 class TableAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    filter_horizontal = ('verbes',)
+    form = TableAdminForm
 
 
 admin.site.register(Verbe, VerbeAdmin)
