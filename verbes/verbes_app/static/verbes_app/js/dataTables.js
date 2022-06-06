705d718e9147a4b6cfca89fa91ab7284d9511403
John <john.t.singer@gmail.com>
2022-06-06 10:24:04 +0200

    Création du fichier bootstrap_modified.css pour modifier légerement le css lorsque c'est nécessaire et creation de dataTables.js script qui controle les datatable

---
+++ verbes/verbes_app/static/verbes_app/js/dataTables.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#example').DataTable({
+        scrollY: '45em',
+        scrollX: true,
+        scrollCollapse: true,
+        paging: false,
+        bFilter: true,
+        bInfo: false,
+    });
+});
