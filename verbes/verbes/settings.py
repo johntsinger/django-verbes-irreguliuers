4d67fb3e6d09a47bae1d358bf4b590d51d96fce8
John <john.t.singer@gmail.com>
2022-06-06 13:01:01 +0200

    Ajout du path vers le processeur de context dans l'option 'context_processor' du reglage TEMPLATES dans settings.py

--- verbes/verbes/settings.py
+++ verbes/verbes/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'verbes_app.context_processors.tables',
             ],
         },
     },
