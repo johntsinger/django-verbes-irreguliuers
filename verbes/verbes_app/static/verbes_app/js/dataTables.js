cc856cae2646be4b391b64b5fa94ffa46d858977
John <john.t.singer@gmail.com>
2022-06-15 18:33:27 +0200

    Creation des fonctions de filtrage qui gèrent le groupe de boutons et insertion du groupe de boutons dans la dataTable + modification du text dans la dataTable

--- verbes/verbes_app/static/verbes_app/js/dataTables.js
+++ verbes/verbes_app/static/verbes_app/js/dataTables.js
@@ -1,3 +1,4 @@
+// dataTables configuration
 $(document).ready(function () {
     $('#example').DataTable({
         scrollY: '45em',
@@ -6,5 +7,78 @@ $(document).ready(function () {
         paging: false,
         bFilter: true,
         bInfo: false,
+        language: {
+            zeroRecords: 'Aucune donnée à afficher',
+            search: 'Rechercher : ',
+        },
     });
+    // Adding #button-group to #example_filter div
+    $('#example_filter').append($('#button-group'));
+});
+
+// Functions that set action for each button of button-group
+$('#success').on('click', function () {
+    // Remove active class for the other buttons
+    $('#unsuccess').removeClass('active')
+    $('#not-done').removeClass('active')
+    $.fn.dataTable.ext.search.pop()
+
+    // Make the button switchable and add a filter function
+    if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+        $.fn.dataTable.ext.search.pop()
+    } else {
+        $(this).addClass('active');
+        $.fn.dataTable.ext.search.push(
+            function(settings, data, dataIndex) {
+                var table = $('#example').DataTable();
+                return $(table.row(dataIndex).node()).hasClass('success');
+            }
+        );
+    };
+    // Display results
+    var table = $('#example').DataTable();
+    table.draw();
+});
+
+$('#unsuccess').on('click', function () {
+    $('#success').removeClass('active')
+    $('#not-done').removeClass('active')
+    $.fn.dataTable.ext.search.pop()
+
+    if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+        $.fn.dataTable.ext.search.pop()
+    } else {
+        $(this).addClass('active');
+        $.fn.dataTable.ext.search.push(
+            function(settings, data, dataIndex) {
+                var table = $('#example').DataTable();
+                return $(table.row(dataIndex).node()).hasClass('unsuccess');
+            }
+        );
+    };
+    var table = $('#example').DataTable();
+    table.draw();
+});
+
+$('#not-done').on('click', function () {
+    $('#success').removeClass('active')
+    $('#unsuccess').removeClass('active')
+    $.fn.dataTable.ext.search.pop()
+
+    if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+        $.fn.dataTable.ext.search.pop()
+    } else {
+        $(this).addClass('active');
+        $.fn.dataTable.ext.search.push(
+            function(settings, data, dataIndex) {
+                var table = $('#example').DataTable();
+                return !$(table.row(dataIndex).node()).hasClass('unsuccess') && !$(table.row(dataIndex).node()).hasClass('success');
+            }
+        );
+    };
+    var table = $('#example').DataTable();
+    table.draw();
 });
