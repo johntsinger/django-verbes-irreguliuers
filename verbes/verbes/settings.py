01ea9753931dcb8269020280c1b9d995fa3c9726
John <john.t.singer@gmail.com>
2022-07-05 10:15:58 +0200

    Ajout de l'application authentication dans installed_apps et ajout du nouveau model d'authentification dans auth_user_model

--- verbes/verbes/settings.py
+++ verbes/verbes/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from os.path import join
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
     'verbes_app',
 ]
 
@@ -126,8 +126,14 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# locale path for internationalization
+# Locale path for internationalization
+# https://docs.djangoproject.com/en/4.0/ref/settings/#locale-paths
 
 LOCALE_PATHS = [
-    join(BASE_DIR, 'locale'),
+    BASE_DIR.joinpath('locale'),
 ]
+
+# Model used for authentication
+# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-user-model
+
+AUTH_USER_MODEL = 'authentication.User'
