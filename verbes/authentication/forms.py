896354503dc20c6a5bc0ec10a265eb5d8981db94
John <john.t.singer@gmail.com>
2022-09-07 10:07:46 +0200

    Ajout du formulaire d'inscription SignupForm

--- verbes/authentication/forms.py
+++ verbes/authentication/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 from authentication.models import User
 
 
@@ -8,3 +10,18 @@ class LoginForm(forms.Form):
         max_length=63,
         widget=forms.PasswordInput,
         )
+
+
+class SignupForm(UserCreationForm):
+    class Media:
+        css = {
+            'all': ('verbes_app/css/custom_forms.css',)
+        }
+
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+        fields = ('username', 'email')
+        widgets = {
+            'username': forms.fields.TextInput(
+                attrs={'placeholder': User._meta.get_field('username').verbose_name})
+        }
