90cf1c1aca4b54f063e94a7a657c415b8367643b
John <john.t.singer@gmail.com>
2022-09-07 12:11:35 +0200

    Ajout des model UserTable, UserVerbe et UserProfile et modification des autre models pour s'accorder avec les nouveaux models et ajout d'un foction qui receptionne les signals m2m changed lors de l'ajout et la suppression d'une Table dans l'UserProfile pour créer et supprimer les UserTable correspondants et d'un fonction qui receptionne les signals m2m changed lors de l'ajout et la suppression d'un Verbe dans une Table pour créer et supprimer les UserTable correspondants

--- verbes/verbes_app/models.py
+++ verbes/verbes_app/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import BaseUserManager
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
 
 
 class Verbe(models.Model):
@@ -6,8 +10,6 @@ class Verbe(models.Model):
     preterit = models.fields.CharField(max_length=50, blank=True)
     participe_passe = models.fields.CharField(max_length=50, blank=True)
     francais = models.fields.CharField(max_length=50)
-    done = models.fields.BooleanField(default=False)
-    success = models.fields.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.present} {self.preterit} {self.participe_passe} \
@@ -16,21 +18,74 @@ class Verbe(models.Model):
 
 class Table(models.Model):
     name = models.fields.CharField(max_length=30)
-    verbes = models.ManyToManyField(Verbe, related_name='tables',
-        through='VerbeList')
+    verbes = models.ManyToManyField(Verbe)
     default = models.fields.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.name}'
 
 
-class VerbeList(models.Model):
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE)
+    tables = models.ManyToManyField(Table)
+    verbes = models.ManyToManyField(Verbe, related_name='verbes',
+        through='UserVerbe')
+
+    def __str__(self):
+        return f'{self.user.username if self.user.username else self.user.email}'
+
+
+class UserTable(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     verbe = models.ForeignKey(Verbe, on_delete=models.CASCADE)
     table = models.ForeignKey(Table, on_delete=models.CASCADE,
-        related_name='table_verbes')
+        related_name='user_table')
     done = models.fields.BooleanField(default=False)
     success = models.fields.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.verbe.present} {self.verbe.preterit} \
-{self.verbe.participe_passe} {self.verbe.francais}'
+        return f'{self.verbe}'
+
+
+class UserVerbe(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    verbe = models.ForeignKey(Verbe, on_delete=models.CASCADE,
+        related_name='user_verbes')
+    done = models.fields.BooleanField(default=False)
+    success = models.fields.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.verbe}'
+
+
+@receiver(m2m_changed, sender=UserProfile.tables.through, dispatch_uid="user_profile_changed")
+def user_profile_changed(sender, instance, action, pk_set, **kwargs):
+    """Create or delete UserTable if Table objects are added or removed from UserProfile.tables m2m"""
+    if action == 'post_add':
+        # instance is UserProfile object
+        for table in instance.tables.filter(id__in=pk_set):
+            for verbe in table.verbes.all():
+                UserTable.objects.get_or_create(user_profile=instance,
+                    table=table, verbe=verbe)
+    if action == 'pre_remove':
+        instance.usertable_set.filter(user_profile=instance,
+            table__id__in=pk_set).delete()
+        #for table in instance.tables.filter(id__in=pk_set):
+        #UserTable.objects.filter(user_profile=instance,
+        #    table__id__in=pk_set).delete()
+
+@receiver(m2m_changed, sender=Table.verbes.through, dispatch_uid="table_changed")
+def table_changed(sender, instance, action, pk_set, **kwargs):
+    """Create or delete UserTable if Verbe object are added or removed from Table.verbes m2m"""
+    if action == 'post_add':
+        # instance is Table object
+        for verbe in instance.verbes.filter(id__in=pk_set):
+            for userprofile in instance.userprofile_set.all():
+                UserTable.objects.get_or_create(user_profile=userprofile,
+                    table=instance, verbe=verbe)
+    if action == 'pre_remove':
+        for verbe in instance.verbes.filter(id__in=pk_set):
+            for userprofile in instance.userprofile_set.all():
+                UserTable.objects.filter(user_profile=userprofile,
+                    table=instance, verbe=verbe).delete()
