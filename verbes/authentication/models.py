296dd1456b8141de59896f4e419720af14673dfd
John <john.t.singer@gmail.com>
2022-09-07 10:21:04 +0200

    Ajout d'une fonction receptrice du signal post_save lors de la création d'un utilisateur qui créé un UserProfile et lui ajoute les verbes et le tables associés

--- verbes/authentication/models.py
+++ verbes/authentication/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.db import models
+from verbes_app.models import UserProfile, Verbe, Table
 
 
 class MyUserManager(BaseUserManager):
@@ -47,3 +50,12 @@ class User(AbstractUser):
     def __str__(self):
         return f'{self.username if self.username else self.email}' 
 
+
+@receiver(post_save, sender=User, dispatch_uid="user_profile")
+def create_user_profile(sender, instance, **kwargs):
+    """Create UserProfile for each created User"""
+    user_profile, create = UserProfile.objects.get_or_create(user=instance)
+    verbes = list(Verbe.objects.all())
+    user_profile.verbes.add(*verbes)
+    tables = list(Table.objects.filter(default=True))
+    user_profile.tables.add(*tables)
