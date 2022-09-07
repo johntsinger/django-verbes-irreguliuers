a128a5aaa5f321e19d0daab602dc553ab99ea3dc
John <john.t.singer@gmail.com>
2022-09-07 12:17:15 +0200

    Modification du context processor pour correspondre avec les nouveaux models

--- verbes/verbes_app/context_processors.py
+++ verbes/verbes_app/context_processors.py
@@ -1,5 +1,20 @@
-from verbes_app.models import Table
+from verbes_app.models import Table, UserProfile, Verbe, UserVerbe, UserTable
+
 
 def tables(request):
-    tables = Table.objects.all()
+    if request.user.is_authenticated:
+        user_profile = UserProfile.objects.get(user=request.user)
+        tables = user_profile.tables.all()
+    else:
+        tables = Table.objects.filter(default=True)
+    
     return {'tables': tables}
+
+def verbes(request):
+    if request.user.is_authenticated:
+        user_profile = UserProfile.objects.get(user=request.user)
+        verbes = UserVerbe.objects.filter(user_profile=user_profile)
+    else:
+        verbes = Verbe.objects.all()
+    
+    return {'verbes': verbes}
