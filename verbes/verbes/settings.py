035db54e317e52c6b81525e8c638abf4a1a08376
John <john.t.singer@gmail.com>
2022-10-31 11:48:38 +0100

    Ajout de STATICFILES_DIRS pour definir le path vers les fichiers statics qui se trouvent à la racine du projet

--- verbes/verbes/settings.py
+++ verbes/verbes/settings.py
@@ -127,6 +127,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Add static files directory for static files in project's root
+
+STATICFILES_DIRS = (
+    BASE_DIR.joinpath('static'),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
