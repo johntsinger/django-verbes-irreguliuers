9b574927640d3553521c2b4003964b08ce5408bf
John <john.t.singer@gmail.com>
2022-10-31 11:37:00 +0100

    Ajout de la vue parameters qui gère les differents formulaires sur la page parameters.html

--- verbes/authentication/views.py
+++ verbes/authentication/views.py
@@ -1,7 +1,11 @@
 from django.conf import settings
-from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth import (authenticate, login, logout,
+    update_session_auth_hash)
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from authentication.forms import LoginForm, SignupForm
+from authentication.forms import (LoginForm, SignupForm,
+    MyPasswordChangeForm, EmailChangeForm, DeleteAccountForm)
 from verbes_app.models import UserProfile, Verbe, Table, UserTable
 
 
@@ -42,3 +46,52 @@ def signup_page(request):
     return render(request,
         'authentication/signup.html',
         {'form': form})
+
+@login_required
+def parameters(request):
+    if request.method == "POST":
+        if 'change_password' in request.POST:
+            password_form = MyPasswordChangeForm(request.user, request.POST)
+            if password_form.is_valid():
+                user = password_form.save()
+                update_session_auth_hash(request, user) # Otherwise the user’s auth session will be invalidated and she/he will have to log in again.
+                messages.success(request,
+                    'Your password was successfully updated !')
+                return redirect('parameters')
+            else:
+                messages.error(request, 'Please correct the errors below.')
+        if 'change_email' in request.POST:
+            email_form = EmailChangeForm(request.POST,
+                initial={'current_email': request.user.email})
+            if email_form.is_valid():
+                user = request.user
+                user.email = email_form.cleaned_data['new_email']
+                user.save()
+                messages.success(request,
+                    'Your email was successfully updated !')
+                return redirect('parameters')
+            else:
+                messages.error(request, 'Please correct the errors below.')
+        if 'delete-account' in request.POST:
+            delete_form = DeleteAccountForm(request.POST)
+            if delete_form.is_valid():
+                user = authenticate(
+                    email=request.user.email,
+                    password=delete_form.cleaned_data['password'],
+                )
+                if user is not None:
+                    request.user.delete()
+                    return redirect('/')
+                else:
+                    messages.error(request,
+                        'The password you entered does not match your password.')
+                    return redirect('parameters')
+    else:
+        password_form = MyPasswordChangeForm(request.user)
+        email_form = EmailChangeForm(
+            initial={'current_email': request.user.email})
+        delete_form = DeleteAccountForm()
+    return render(request,
+        'authentication/parameters.html',
+        {'password_form': password_form, 'email_form': email_form, 
+        'email': request.user.email, 'delete_form': delete_form})
