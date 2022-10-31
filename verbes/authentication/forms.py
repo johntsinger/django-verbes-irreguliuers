1b8e21ce3b652f513d4ba18f160c33e5c0c85563
John <john.t.singer@gmail.com>
2022-10-31 10:44:13 +0100

    Modification de la fonction init de SignupForm pour ajouter une class 'signup-input' au champ de formulaire pour augmenter la taille des champset ajout des formulaires MyPasswordChangeForm, EmailChangeForm et DeleteAccountForm

--- verbes/authentication/forms.py
+++ verbes/authentication/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from authentication.models import User
 
 
@@ -13,6 +13,7 @@ class LoginForm(forms.Form):
 
 
 class SignupForm(UserCreationForm):
+
     class Media:
         css = {
             'all': ('verbes_app/css/custom_forms.css',)
@@ -25,3 +26,24 @@ class SignupForm(UserCreationForm):
             'username': forms.fields.TextInput(
                 attrs={'placeholder': User._meta.get_field('username').verbose_name})
         }
+
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
+        # Adding class to signup form input
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'signup-input'
+
+
+class MyPasswordChangeForm(PasswordChangeForm):
+    change_password = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+
+
+class EmailChangeForm(forms.Form):
+    change_email = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+    current_email = forms.EmailField(disabled=True)
+    new_email = forms.EmailField()
+
+
+class DeleteAccountForm(forms.Form):
+    password = forms.CharField(max_length=63, widget=forms.PasswordInput)
+    template_name = "authentication/delete_account_form.html"
