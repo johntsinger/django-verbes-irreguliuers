eea1bacdd66d8b8f7a03deb5041fdebd46c0db6e
John <john.t.singer@gmail.com>
2022-11-07 15:16:56 +0100

    Suppression des vues table_delete, table_reset et reset_all intégréés maintenant a verbe_list , table_détail et table_list, modification des objets verbes serialiser dans table_update et table_create pour rajouter le champs verbe et vérifie si le nom de la table est déjà pris dans table_create

--- verbes/verbes_app/views.py
+++ verbes/verbes_app/views.py
@@ -2,63 +2,95 @@ from random import sample
 from django.core import serializers
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from verbes_app.functions import get_results, verify_answer
 from verbes_app.models import Verbe, Table, UserTable, UserProfile, UserVerbe
-from verbes_app.forms import TableForm, VerbeForm
+from verbes_app.forms import (TableForm, VerbeForm, ResetAllForm,
+    ResetListForm, DeleteListForm)
 
 
 def verbe_list(request):
+    reset_all_form = ResetAllForm()
+    if request.user.is_authenticated:
+        user_profile = UserProfile.objects.get(user=request.user)
+        user_verbes = UserVerbe.objects.filter(user_profile=user_profile)
+        user_tables = UserTable.objects.filter(user_profile=user_profile)
+        if request.method == 'POST':
+            if 'reset-all' in request.POST:
+                user_verbes.filter(done=True).update(done=False, success=False)
+                user_tables.filter(done=True).update(done=False, success=False)
+            return redirect('verbe-list')
 
     return render(request,
         "verbes_app/verbe_list.html",
-        )
-
-@login_required
-def reset_all(request):
-    user_profile = UserProfile.objects.get(user=request.user)
-    verbes = UserVerbe.objects.filter(user_profile=user_profile)
-    if request.method == 'POST':
-        if 'envoyer' in request.POST:
-            for verbe in verbes.filter(done=True):
-                verbe.done = False
-                verbe.success = False
-                verbe.save()
-        return redirect('verbe-list')
-
-    return render(request,
-        'verbes_app/reset_all.html')
+        {'reset_all_form': reset_all_form})
 
 def table_detail(request, table_id):
     if request.user.is_authenticated:
         user_profile = UserProfile.objects.get(user=request.user)
         table = user_profile.tables.get(id=table_id)
-        user_table = UserTable.objects.filter(user_profile=user_profile, table=table)
+        user_table = UserTable.objects.filter(user_profile=user_profile,
+            table=table)
+        reset_list_form = ResetListForm(user=request.user, table_id=table_id)
+        delete_list_form = DeleteListForm(user=request.user, table_id=table_id)
+        context = {"table": table, "user_table": user_table,
+            'reset_list_form': reset_list_form,
+            'delete_list_form': delete_list_form}
+        if request.method == 'POST':
+            if 'reset' in request.POST:
+                user_table.filter(done=True).update(done=False, success=False)
+                return redirect('table-detail', table_id)
+            if 'delete' in request.POST:
+                table.delete()
+                return redirect('table-list')
     else:
         table = Table.objects.get(id=table_id)
         user_table = table.verbes.all()
+        context = {"table": table, "user_table": user_table}
 
     return render(request,
         "verbes_app/table_detail.html",
-        {"table": table, "user_table": user_table})
+        context)
 
 def table_list(request):
+    if request.method == 'POST':
+        if 'delete' in request.POST:
+            user_profile = UserProfile.objects.get(user=request.user)
+            table_id = request.POST['table-id']
+            table = user_profile.tables.get(id=table_id)
+            table.delete()
+            return redirect('table-list')
+    else:
+        delete_list_form = DeleteListForm()
     
     return render(request,
         "verbes_app/table_list.html",
-        )
+        {'delete_list_form': delete_list_form})
 
 @login_required
 def table_create(request):
     # creates a json dict of object Verbe with selected fields
     user_profile = UserProfile.objects.get(user=request.user)
-    verbes = serializers.serialize('json', UserVerbe.objects.filter(user_profile=user_profile),
-        fields=('done', 'success'))
+    # Added verbe field to sort verbes in SelectBoxModified.js necessary not
+    # to have an index shift when assigning the success and unsuccess classes
+    # if the UserVerbes objects inside UserProfile have been modified
+    verbes = serializers.serialize('json',
+        UserVerbe.objects.filter(user_profile=user_profile),
+        fields=('verbe', 'done', 'success'))
     if request.method == "POST":
         # Button envoyer
         if 'envoyer' in request.POST:
             form = TableForm(request.POST)
             if form.is_valid():
+                # Check if table's name is already used
+                for table in user_profile.tables.all():
+                    if form.cleaned_data['name'] in table.name:
+                        messages.error(request,
+                            'This name is already used ! Please choose another one.')
+                        return render(request,
+                            'verbes_app/table_create.html',
+                            {'form': form, 'verbes': verbes})
                 table = form.save()
                 user_profile.tables.add(table)
                 return redirect('table-detail', table.id)
@@ -78,8 +110,12 @@ def table_update(request, table_id):
     user_profile = UserProfile.objects.get(user=request.user)
     table = Table.objects.get(id=table_id)
     verbe_in_table = table.verbes.all()
-    verbes = serializers.serialize('json', UserVerbe.objects.filter(user_profile=user_profile),
-        fields=('done', 'success'))
+    # Added verbe field to sort verbes in SelectBoxModified.js necessary not
+    # to have an index shift when assigning the success and unsuccess classes
+    # if the UserVerbes objects inside UserProfile have been modified
+    verbes = serializers.serialize('json',
+        UserVerbe.objects.filter(user_profile=user_profile),
+        fields=('verbe', 'done', 'success')) # verbe' value is 1 to 282
     if request.method == 'POST':
         form = TableForm(request.POST, instance=table)
         if 'envoyer' in request.POST:
@@ -89,41 +125,11 @@ def table_update(request, table_id):
         else:
             return redirect('table-list')
     else:
-        form = TableForm(instance=table)  
+        form = TableForm(instance=table)
 
     return render(request,
         'verbes_app/table_update.html',
-        {'form': form, 'verbes': verbes})
-
-@login_required
-def table_delete(request, table_id):
-    table = Table.objects.get(id=table_id)
-    if request.method == 'POST':
-        if 'envoyer' in request.POST:
-            table.delete()
-
-        return redirect('table-list')
-
-    return render(request,
-        'verbes_app/table_delete.html',
-        {'table': table})
-
-@login_required
-def table_reset(request, table_id):
-    user_profile = UserProfile.objects.get(user=request.user)
-    table = Table.objects.get(id=table_id)
-    user_tables = UserTable.objects.filter(user_profile=user_profile, table=table)
-    if request.method == 'POST':
-        if 'envoyer' in request.POST:
-            for user_table in user_tables.filter(done=True):
-                user_table.done = False
-                user_table.success = False
-                user_table.save()
-        return redirect('table-detail', table_id)
-
-    return render(request,
-        'verbes_app/table_reset.html',
-        {'table': table})
+        {'form': form, 'verbes': verbes, 'table': table})
 
 @login_required
 def exercise(request, table_id):
@@ -156,7 +162,8 @@ def exercise_result(request, table_id):
     # saves the modification in the db
     for i, user_table in enumerate(results['verbes']):
         # get the UserVerbe object
-        verbe = UserVerbe.objects.get(verbe=user_table.verbe, user_profile=user)
+        verbe = UserVerbe.objects.get(verbe=user_table.verbe,
+            user_profile=user)
         # modifies the UserTable object done attribute
         user_table.done = True
         # modifies the UserVerbe object done attribute
